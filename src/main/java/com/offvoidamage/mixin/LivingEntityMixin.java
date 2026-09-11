3b3c4e6a749c41e3d8c58fd552042092cf9cf824
ben45533565-lang <ben45533565@gmail.com>
2026-09-12 00:51:45 +0300

    Add mod metadata

---
+++ src/main/java/com/offvoidamage/mixin/LivingEntityMixin.java
@@ -0,0 +1,19 @@
+package com.offvoidamage.mixin;
+
+import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.LivingEntity;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
+
+@Mixin(LivingEntity.class)
+public class LivingEntityMixin {
+    @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
+    private void offVoidDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
+        if (source.is(DamageTypes.OUT_OF_WORLD)) {
+            cir.setReturnValue(false);
+        }
+    }
+}
