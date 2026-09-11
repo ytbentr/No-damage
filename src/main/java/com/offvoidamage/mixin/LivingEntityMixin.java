0bdd1c01ed5e2e528656a3677f76d545b9c550e6
ben45533565-lang <ben45533565@gmail.com>
2026-09-12 00:59:10 +0300

    Fix void damage type

--- src/main/java/com/offvoidamage/mixin/LivingEntityMixin.java
+++ src/main/java/com/offvoidamage/mixin/LivingEntityMixin.java
@@ -1,7 +1,8 @@
 package com.offvoidamage.mixin;
 
-import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.damagesource.DamageSource;
+import net.minecraft.world.damagesource.DamageTypes;
+import net.minecraft.world.entity.LivingEntity;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -9,10 +10,9 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-
     @Inject(method = "hurt", at = @At("HEAD"), cancellable = true)
     private void offVoidDamage(DamageSource source, float amount, CallbackInfoReturnable<Boolean> cir) {
-        if (source.isOutOfWorld()) {
+        if (source.is(DamageTypes.FELL_OUT_OF_WORLD)) {
             cir.setReturnValue(false);
         }
     }
