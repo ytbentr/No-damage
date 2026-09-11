5ad03728bdbec08d64d93e513bf04839e450e8a4
ben45533565-lang <ben45533565@gmail.com>
2026-09-12 00:43:34 +0300

    Upload mod source

---
+++ src/main/java/com/offvoidamage/OffVoidDamage.java
@@ -0,0 +1,11 @@
+package com.offvoidamage;
+
+import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
+import net.minecraft.entity.damage.DamageTypes;
+
+public class OffVoidDamage implements ModInitializer {
+    @Override
+    public void onInitialize() {
+    }
+}
