f32bbc16efc02c3721d838b88869e91cca40133f
ben45533565-lang <ben45533565@gmail.com>
2026-09-12 00:53:58 +0300

    Fix Minecraft 1.20.1 mappings

--- src/main/java/com/offvoidamage/OffVoidDamage.java
+++ src/main/java/com/offvoidamage/OffVoidDamage.java
@@ -1,8 +1,6 @@
 package com.offvoidamage;
 
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.event.lifecycle.v1.ServerEntityEvents;
-import net.minecraft.entity.damage.DamageTypes;
 
 public class OffVoidDamage implements ModInitializer {
     @Override
